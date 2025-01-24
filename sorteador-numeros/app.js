f6a434b9b124232c3a893ffa134d90be3dd7d7cc
Yves Monma <60709926+Yves-Yu@users.noreply.github.com>
2025-01-23 23:14:12 -0300

    Sorteio de Numeros Aleatórios

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,43 @@
+function sortear(){
+    let quantidade = parseInt(document.getElementById("quantidade").value);
+    let de = parseInt(document.getElementById("de").value);
+    let ate = parseInt(document.getElementById("ate").value);
+
+    let sorteado = [];
+    
+    for(let i = 0; i < quantidade; i++){
+        let numero = obterNumeroAleatorio(de,ate);
+        while(sorteado.includes(numero)){
+            numero = obterNumeroAleatorio(de,ate);
+        }
+
+        sorteado.push(numero);
+    }
+  
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados:  ${sorteado}</label>`
+    alterarStatusBotao();
+}
+
+function reiniciar(){
+    parseInt(document.getElementById("quantidade").value) = 0;
+    parseInt(document.getElementById("de").value) = 0;
+    parseInt(document.getElementById("ate").value) = 0;
+    reesultado.innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+}
+
+function alterarStatusBotao(){
+    let botao = document.getElementById("btn-reiniciar");
+    if(botao.classList.contains("container__botao-desabilitado")){
+        botao.classList.remove("container__botao-desabilitado");
+        botao.classList.add("container__botao")
+    }else{
+        botao.classList.remove("container__botao")
+        botao.classList.add("container__botao-desabilitado")
+    }
+}
+
+function obterNumeroAleatorio(min,max){
+    return parseInt((Math.random() * (max - min + 1)) + min);
+}
